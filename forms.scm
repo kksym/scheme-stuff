a56699b61ac0e8895ea971790bd48e175972aa9d
Kyle K <kyliek24@hotmail.co.uk>
2012-03-21 02:03:39 +0000

    fixed up symbolic simplifier

--- forms.scm
+++ forms.scm
@@ -4,3 +4,18 @@
     ((_ PRED BODY ...)
      (if PRED (begin BODY ...)))))
 
+(define-syntax define-object
+  (syntax-rules (let-members let-methods)
+    ((_ NAME
+       (let-members MEMBERS)
+       (let-methods METHODS))
+     (define 
+
+(define-object stack
+  (let-members ((stk '())))
+  (let-methods
+    ((push! obj) (set! stk (cons obj stk)))
+    ((pop!) (let ((top (car stk)))
+              (set! stk (cdr stk))
+              top))))
+
--- sym.scm
+++ sym.scm
@@ -1,34 +1,52 @@
 (use srfi-1)
 
-; (+ (/ (/ (* 1 10) 1) 1) (/ 6 1)) ==> (+ 10 6)
+(define rules '())
 
-;; NOTE: this only works with s-expressions in the form:
-;;  (OPERATION OPERAND1 OPERAND2)
-;;  ie. (/ 2 1), (* 1 x)
+(define (define-rule op sym)
+  (set! rules (append rules (list (cons op sym)))))
 
-(define (rules expr)
-  (case (car expr)
-    ((/) (if (eq? (caddr expr) 1)
-           (cadr expr)
-           expr))
-    ((*) (cond
-           ((eq? (cadr expr) 1) (caddr expr))
-           ((eq? (caddr expr) 1) (cadr expr))
-           (else expr)))))
+(define-syntax define-rule
+  (syntax-rules ()
+    ((_ (OP . EXPR) BODY ...)
+     (set! rules (append rules (list (cons (quote OP) (lambda EXPR BODY ...))))))))
 
-;; TODO: edit this to avoid state mutation of ACCUM with APPEND!
-;; TODO: clean up 
-(define accum '())
+(define (identity x) x)
 
-(define (id-simplify expr last)
-  (let ((func (lambda ()
-                (map (lambda (x)
-                       (if (list? x)
-                         (append! accum (rules x))
-                         x)) expr))))
-    (let loop ((result (func))
-               (lastresult '()))
-      (if (equal? result last)
-        result
-        (id-simplify result result)))))
+(define (lookup-rule sym)
+  (let ((rule (assq sym rules)))
+    (if rule
+      (cdr rule)
+      identity)))
+
+(define (apply-rule expr)
+  (if (list? expr)
+    ((lookup-rule (car expr)) expr)
+    expr))
+
+;; RULE DEFINITIONS
+
+(define-rule (/ expr) 
+  (cond
+    ((eq? (caddr expr) 1) (cadr expr)) ; X / 1 = X
+    ((eq? (cadr expr) (caddr expr)) 1) ; X / X = 1
+    (else expr)))
+
+(define-rule (* expr) 
+  (cond
+    ((eq? (cadr expr) 1) (caddr expr)) ; 1 * X = X
+    ((eq? (caddr expr) 1) (cadr expr)) ; X * 1 = X
+    (else expr)))
+
+;; END RULE DEFINITIONS
+
+;; (simplify '(+ (/ (/ (* 1 10) 1) 1) (/ 6 1)))
+;;  ==> (+ 10 6)
+
+(define (simplify expr)
+  (apply-rule
+    (map (lambda (sub)
+      (let ((nsub (apply-rule sub)))
+        (if (list? nsub)
+          (simplify nsub)
+          nsub))) expr)))
 
