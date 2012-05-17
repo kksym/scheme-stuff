d627a0f46d661ed2dc4adcfe4d96d6944d4b63db
Kyle K <kyliek24@hotmail.co.uk>
2012-05-18 00:57:41 +0100

    fixes and stuff

--- sym.scm
+++ sym.scm
@@ -34,6 +34,17 @@
     ((eq? (caddr expr) 1) (cadr expr)) ; X * 1 = X
     (else expr)))
 
+(define-rule (+ expr)
+  (cond
+    ((eq? (cadr expr) 0) (caddr expr)) ; 0 + X = X
+    ((eq? (caddr expr) 0) (cadr expr)) ; X + 0 = X
+    (else expr)))
+
+(define-rule (exp expr)
+  (cond
+    ((eq? (caddr expr) 1) (cadr expr)) ; X^1 = X
+    (else expr)))
+
 ;; END RULE DEFINITIONS
 
 ;; (simplify '(+ (/ (/ (* 1 10) 1) 1) (/ 6 1)))
@@ -47,4 +58,29 @@
                (simplify nsub)
                nsub)))
          expr)))
+
+
+;; 3x^3 + 12x
+;;   ==>
+;; 9x^2 + 12
+;;
+;; (+ (exp (* 3 x) 3) (* 12 x))
+;;   ==>
+;; (+ (exp (* 9 x) 2) 12)
+
+(define (differ-exp expr) '())
+
+(define (differentiate expr) '())
+
+;; 3x + 12
+;;   ==>
+;; 3x^2
+;; ---- + 12x + c
+;;   2
+;;
+;; (+ (* 3 x) 12)
+;;   ==>
+;; (+ (+ (/ (exp (* 3 x) 2) 2) (* 12 x)) c)
+
+(define (integrate expr) '())
 
