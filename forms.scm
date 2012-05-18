307a7bd1e7e9685e8b1eff84f8557e78fbd70786
Kyle K <kyliek24@hotmail.co.uk>
2012-05-18 01:41:41 +0100

    fixed up forms.scm

--- forms.scm
+++ forms.scm
@@ -4,18 +4,3 @@
     ((_ PRED BODY ...)
      (if PRED (begin BODY ...)))))
 
-(define-syntax define-object
-  (syntax-rules (let-members let-methods)
-    ((_ NAME
-       (let-members MEMBERS)
-       (let-methods METHODS))
-     (define 
-
-(define-object stack
-  (let-members ((stk '())))
-  (let-methods
-    ((push! obj) (set! stk (cons obj stk)))
-    ((pop!) (let ((top (car stk)))
-              (set! stk (cdr stk))
-              top))))
-
